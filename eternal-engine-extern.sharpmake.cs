a28186b5fcf7a6da3bdc9cce314c706272c7954e
Daniel Quach <daniel@quach.info>
2024-01-02 22:47:28 +0100

    Sharpmake

--- eternal-engine-extern.sharpmake.cs
+++ eternal-engine-extern.sharpmake.cs
@@ -19,10 +19,17 @@ namespace EternalEngine
 			"include",
 		};
 
+		static readonly string[] NonWindowsExclusions = new string[] {
+			"DirectXTex",
+			"DirectX-Headers",
+			"NVIDIANsightAftermath",
+			"renderdoc",
+		};
+
 		public EternalEngineExternProject()
 			: base("extern")
 		{
-			SourceFilesExcludeRegex.Add(new string[] {
+			SourceFilesExcludeRegex.AddRange(new string[] {
 				@"(.*)imgui\\(.*)\\(.*)",
 				@"(.*)rapidjson\\example\\(.*)",
 				@"(.*)rapidjson\\test\\(.*)",
@@ -34,22 +41,56 @@ namespace EternalEngine
 			});
 		}
 
+		public override void AdditionalFiltering(Strings InSourceFiles, ref Strings InOutSourceFilesExclude)
+		{
+			base.AdditionalFiltering(InSourceFiles, ref InOutSourceFilesExclude);
+
+			for (int NonWindowsExclusionIndex = 0; NonWindowsExclusionIndex < NonWindowsExclusions.Length; ++NonWindowsExclusionIndex)
+			{
+				NonWindowsExclusions[NonWindowsExclusionIndex] = NonWindowsExclusions[NonWindowsExclusionIndex].ToLower();
+			}
+
+			for (int ConfigurationIndex = 0; ConfigurationIndex < Configurations.Count; ++ConfigurationIndex)
+			{
+				if (Configurations[ConfigurationIndex].Platform != Platform.win64 && Configurations[ConfigurationIndex].Platform != Platform.win32)
+				{
+					foreach (string CurrentSourceFile in InSourceFiles)
+					{
+						foreach (string CurrentNonWindowsExclusion in NonWindowsExclusions)
+						{
+							if (CurrentSourceFile.Contains(CurrentNonWindowsExclusion))
+							{
+								InOutSourceFilesExclude.Add(CurrentSourceFile);
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+
 		public override void ConfigureAll(Configuration InConfiguration, Target InTarget)
 		{
 			base.ConfigureAll(InConfiguration, InTarget);
 
 			// Include paths
-			InConfiguration.IncludePaths.Add(new string[] {
-				@"[conf.ProjectPath]\DirectX-Headers\include",
-				@"[conf.ProjectPath]\DirectXTex\DirectXTex",
+			InConfiguration.IncludePaths.AddRange(new string[] {
 				@"[conf.ProjectPath]\libtga\include",
-				@"[conf.ProjectPath]\NVIDIANsightAftermath\include",
-				@"[conf.ProjectPath]\renderdoc\include",
 				@"$(SolutionDir)eternal-engine-core\include",
 			});
 
+			if (InTarget.Platform == Platform.win64 || InTarget.Platform == Platform.win32)
+			{
+				InConfiguration.IncludePaths.AddRange(new string[] {
+					@"[conf.ProjectPath]\DirectX-Headers\include",
+					@"[conf.ProjectPath]\DirectXTex\DirectXTex",
+					@"[conf.ProjectPath]\NVIDIANsightAftermath\include",
+					@"[conf.ProjectPath]\renderdoc\include",
+				});
+			}
+
 			// Include system paths
-			InConfiguration.IncludeSystemPaths.Add(new string[] {
+			InConfiguration.IncludeSystemPaths.AddRange(new string[] {
 				@"$(SolutionDir)eternal-engine-extern\DirectX-Headers\include",
 				EternalEngineSettings.VulkanPath + @"\Include",
 			});
